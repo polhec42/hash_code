29a32eb16195532048053f595f9d9271877a470f
grzin <36706979+grzin@users.noreply.github.com>
2019-02-28 19:23:33 +0100

    Add files via upload

--- Resitev.java
+++ Resitev.java
@@ -1,5 +1,37 @@
 import java.util.*;
+import java.io.*;
+
+
 public class Resitev{
-  public static void main(String[] args){};
+	private static class Slika{
+		public HashSet<String> tags;
+		public char ori;
+		public int ID;
+		
+		public Slika(int ID, char orientation, HashSet<String> tags){
+				this.orientation = orientation;
+				this.ID = ID;
+				this.tags = tags;
+		}
+		
+		
+		
+	}
+	
+	/*private static class Slide{
+		public Slika 
+		public String[] tags;
+		public int ID1;
+		public int ID2;
+		
+		public Slide(int ID, Slika s){
+			
+		}
+		
+	}*/
+	
+	public static void main(String[] args){
+	  
+  }
 }
 
