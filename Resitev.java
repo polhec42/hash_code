b3a4ccdd91d51fc4951490203b30ca9b632d5d42
polhec42 <zan.magerl@hotmail.com>
2019-02-28 19:24:17 +0100

    Reader

--- Resitev.java
+++ Resitev.java
@@ -1,5 +1,32 @@
+import java.io.BufferedReader;
+import java.io.*;
 import java.util.*;
 public class Resitev{
-  public static void main(String[] args){};
+  
+  public static void main(String[] args) throws IOException{
+
+    BufferedReader br = new BufferedReader(new FileReader(args[0]));
+
+    int numberOfPhotos = Integer.parseInt(br.readLine());
+
+    Slika slike = new Slika[numberOfPhotos];
+
+    for(int i = 0; i < slike.length; i++){
+      String[] data = br.readLine().split(" ");
+      char usmerjenost = (char)Integer.parseInt(data[0]);
+      int numberOfTags = Integer.parseInt(data[1]);
+      
+      HashSet<String> set = new HashSet<>();
+      for(int j = 2; j < data.length; j++){
+        set.add(data[j]);
+      }
+
+      Slika novaSlika = new Slika(i, usmerjenost, set);
+    
+    }
+
+  }
+
+  
 }
 
